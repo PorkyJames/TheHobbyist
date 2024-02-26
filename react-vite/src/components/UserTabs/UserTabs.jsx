5f59a88612db959152035caab988c448d5790595
PorkyJames <kwanjko17@gmail.com>
2024-02-26 13:25:02 -0800

    Uncommented out the component as that caused an internal server error

--- react-vite/src/components/UserTabs/UserTabs.jsx
+++ react-vite/src/components/UserTabs/UserTabs.jsx
@@ -1,23 +1,23 @@
 import './UserTabs.css'
 
-// function UserTabs({ activeTab, setActiveTab }) {
+function UserTabs({ activeTab, setActiveTab }) {
 
-//     return (
-//         <div className="user-tabs">
-//             {/* <button
-//                 onClick={() => setActiveTab('hobbies')}
-//                 className={activeTab === 'hobbies' ? 'active' : ''}
-//             >
-//                 Hobbies
-//             </button> */}
-//             {/* <button
-//                 onClick={() => setActiveTab('reviews')}
-//                 className={activeTab === 'reviews' ? 'active' : ''}
-//             >
-//                 Reviews
-//             </button> */}
-//         </div>
-//     );
-// }
+    return (
+        <div className="user-tabs">
+            {/* <button
+                onClick={() => setActiveTab('hobbies')}
+                className={activeTab === 'hobbies' ? 'active' : ''}
+            >
+                Hobbies
+            </button> */}
+            {/* <button
+                onClick={() => setActiveTab('reviews')}
+                className={activeTab === 'reviews' ? 'active' : ''}
+            >
+                Reviews
+            </button> */}
+        </div>
+    );
+}
 
-// export default UserTabs;
+export default UserTabs;
